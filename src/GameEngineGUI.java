212b3b78ca21182115242df7bbd562c26189c920
Alastair@alastairdewar.co.uk <alastair@dewar-server.(none)>
2009-05-26 20:19:28 +0100

    Added a window listener to kill the GUI

--- src/GameEngineGUI.java
+++ src/GameEngineGUI.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
 public class GameEngineGUI {
@@ -46,5 +47,50 @@ public class GameEngineGUI {
     	int y = (int) (screenSize.height - size.getHeight()/2);
     	int x = (int) (screenSize.width - size.getWidth()/2);
     	frame.setLocation(x, y);
+    	frame.addWindowListener(new MyWindowListener());
    }
+    
+
+}
+class MyWindowListener implements WindowListener {
+
+	  public void windowClosing(WindowEvent arg0) {
+	    System.exit(0);
+	  }
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
