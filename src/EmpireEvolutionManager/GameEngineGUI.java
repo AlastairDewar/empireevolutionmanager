225dd06604843a09a3fa39564fb7fa3a21260690
AlastairDewar <Alastair@alastairdewar.co.uk>
2009-06-02 19:41:16 +0100

    Added an actionListener for the buttons on the GUI

--- src/EmpireEvolutionManager/GameEngineGUI.java
+++ src/EmpireEvolutionManager/GameEngineGUI.java
@@ -4,11 +4,12 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
-public class GameEngineGUI {
+public class GameEngineGUI implements ActionListener {
 
 	private GameEngine engine;
 	
 	private JFrame frame;
+	private JLabel menu_name;
 	
 	public GameEngineGUI(GameEngine newEngine) {
 		engine = newEngine;
@@ -27,7 +28,8 @@ public class GameEngineGUI {
     	p1.add(new JLabel("Please bare in mind updates are live before applying any changes."));
     	// etc
     	JPanel p2 = new JPanel(new GridLayout(9,1, 0, 5));
-    	p2.add(new JLabel("              Main Menu"));
+    	menu_name = new JLabel("              Main Menu");
+    	p2.add(menu_name);
     	p2.add(new JButton("News"));
     	p2.add(new JButton("Buildings"));
     	p2.add(new JButton("Research Technologies"));
@@ -35,7 +37,9 @@ public class GameEngineGUI {
     	p2.add(new JButton("Members"));
     	p2.add(new JButton("Private Messaging"));
     	p2.add(new JLabel(""));
-    	p2.add(new JButton("Quit"));
+    	JButton quit_button = new JButton("Quit");
+    	quit_button.addActionListener(this);
+    	p2.add(quit_button);
     	contentPane.add(p1);
     	/* Space between panels */
     	contentPane.add(new JPanel());
@@ -51,6 +55,13 @@ public class GameEngineGUI {
     	frame.setLocation(x, y);
     	frame.addWindowListener(new MyWindowListener());
    }
+
+	public void actionPerformed(ActionEvent arg0) {
+		if(arg0.getActionCommand().equalsIgnoreCase("Quit")){
+			System.out.println("Terminating. Goodbye.");
+			System.exit(0);
+		}
+	}
 
 }
 class MyWindowListener implements WindowListener {
